198be6e93fd246c185e115f3335d5521cf1e74a2
artem.taran <artem.taran@yahoo.com>
2019-10-27 10:15:41 +0100

    adds files

---
+++ js/index.js
@@ -0,0 +1,34 @@
+$('#css').LineProgressbar({
+	
+	percentage: 90
+	
+});
+$('#js').LineProgressbar({
+	
+	percentage: 80
+	
+});
+$('#jq').LineProgressbar({
+	
+	percentage: 70
+	
+});
+$('#img').LineProgressbar({
+	
+	percentage: 75
+	
+});
+$('#bundler').LineProgressbar({
+	
+	percentage: 72
+	
+});
+$('#git').LineProgressbar({
+	
+	percentage: 78
+	
+});
+
+
+
+	
